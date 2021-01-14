f03b9d891b55fc60bbfd1653b5571a5e6524b4b7
Viktoryia Nelia <75176786+vikylkinnelya@users.noreply.github.com>
2021-01-14 20:00:23 +0300

    menu is ended

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
 import {
   Layout,
   Input,
   Row,
   Col,
-  Card, Space,
+  Card,
+  Space,
   Tag,
   Spin,
   Alert,
+  Button,
   Modal,
   Typography,
   Menu,
@@ -26,97 +30,10 @@ import {
 import './app.css';
 
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 const { Search } = Input;
 const { Meta } = Card;
 const TextTitle = Typography.Title;
-
-
-const SearchBox = () => {
-
-  return (
-    <Row marginTop='150px' justify='center'>
-      <Col span={15} >
-        <Search style={{ marginTop: 12, type:'flex' }}
-          placeholder="enter movie, series, episode name"
-          size="large"
-          onSearch={value => console.log(value)}
-        />
-      </Col>
-    </Row>
-
-  )
-}
-
-const MenuBox = () => {
-
-
-  return (
-    <>
-      <Menu
-        style={{
-          width: 200,
-          height: '100vh',
-          background: '#212121',
-          color: '#D6D9DC',
-          paddingTop: '60px',
-          paddingLeft: '20px',
-          fontSize: '26px',
-        }}
-        defaultSelectedKeys={['1']}
-
-      >
-        <Menu.Item
-          key="1"
-          className="customclass"
-          icon={<VideoCameraOutlined style={{ fontSize: '20px' }} />}>
-          films
-        </Menu.Item>
-        <Menu.Item
-          key="2"
-          className="customclass"
-          style={{ marginTop: '10px' }}
-          icon={<VideoCameraOutlined style={{ fontSize: '20px' }} />}>
-          serials
-        </Menu.Item>
-        <Menu.Item
-          key="3"
-          className="customclass"
-          style={{ marginTop: '20px' }}
-          icon={<EyeOutlined style={{ fontSize: '20px' }} />}>
-          watched
-        </Menu.Item>
-        <Menu.Item
-          key="4"
-          className="customclass"
-          style={{ marginTop: '10px' }}
-          icon={<EyeInvisibleOutlined style={{ fontSize: '20px' }} />}>
-          to watch
-        </Menu.Item>
-        <Menu.Item
-          key="5"
-          className="customclass"
-          style={{ marginTop: '20px' }}
-          icon={<HeartOutlined style={{ fontSize: '20px' }} />}>
-          liked
-        </Menu.Item>
-        <Menu.Item
-          key="6"
-          className="customclass"
-          icon={<LikeOutlined style={{ fontSize: '20px' }} />}>
-          good
-        </Menu.Item>
-        <Menu.Item
-          key="7"
-          className="customclass"
-          icon={<DislikeOutlined style={{ fontSize: '20px' }} />}>
-          bad
-        </Menu.Item>
-
-      </Menu>
-    </>
-  )
-}
 
 function App() {
 
@@ -130,17 +47,89 @@ function App() {
    const [detailRequest, setDetailRequest] = useState(false); //отображение загрузчика
  
   */
+  const [collapsed, setCollapsed] = useState(false); //
+
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed)
+  }
 
   return (
     <div className='App'>
-      <Layout className='Layout'>
-        <Sider className='sider'>
-          <MenuBox type='flex' />
+      <Layout className='Layout' style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          onCollapse={toggleCollapsed}
+        >
+          <div className="logo" />
+          <Menu
+            style={{
+              height: '100vh',
+              background: '#212121',
+              color: '#D6D9DC',
+              paddingTop: '60px',
+              paddingLeft: '20px',
+              borderRight: '0px',
+              fontSize: '26px',
+            }}
+            
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            inlineCollapsed={collapsed}
+          >
+            <Menu.Item
+              key="1"
+              mode="inline"
+              className="customclass"
+              icon={<VideoCameraOutlined style={{ fontSize: '20px' }} />}>
+              films
+        </Menu.Item>
+            <Menu.Item
+              key="2"
+              className="customclass"
+              style={{ marginTop: '10px' }}
+              icon={<VideoCameraOutlined style={{ fontSize: '20px' }} />}>
+              serials
+        </Menu.Item>
+            <Menu.Item
+              key="3"
+              className="customclass"
+              style={{ marginTop: '20px' }}
+              icon={<EyeOutlined style={{ fontSize: '20px' }} />}>
+              watched
+        </Menu.Item>
+            <Menu.Item
+              key="4"
+              className="customclass"
+              style={{ marginTop: '10px' }}
+              icon={<EyeInvisibleOutlined style={{ fontSize: '20px' }} />}>
+              to watch
+        </Menu.Item>
+            <Menu.Item
+              key="5"
+              className="customclass"
+              style={{ marginTop: '20px' }}
+              icon={<HeartOutlined style={{ fontSize: '20px' }} />}>
+              liked
+        </Menu.Item>
+            <Menu.Item
+              key="6"
+              className="customclass"
+              icon={<LikeOutlined style={{ fontSize: '20px' }} />}>
+              good
+        </Menu.Item>
+            <Menu.Item
+              key="7"
+              className="customclass"
+              icon={<DislikeOutlined style={{ fontSize: '20px' }} />}>
+              bad
+        </Menu.Item>
+
+          </Menu>
         </Sider>
         <Layout className='layout'>
           <Header style={{ background: '#FFD500' }}>
             <SearchBox />
-
           </Header>
           <Content>
             content
@@ -151,9 +140,32 @@ function App() {
         </Layout>
 
       </Layout>
-    </div>
+    </div >
   )
 
 }
+
+const SearchBox = () => {
+
+  return (
+    <Row marginTop='150px' justify='center'>
+      <Col span={15} >
+        <Search style={{ marginTop: 12, type: 'flex' }}
+          placeholder="enter movie, series, episode name"
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </Col>
+    </Row>
+
+  )
+}
+
+
+
+
+
+
+
 
 export default App;
