08cfb9220951ee69678bf58bba5d009e5986a2ff
Viktoryia Nelia <vikylkinnelya@gmail.com>
2021-03-31 16:45:32 +0300

    modifying starting page

--- src/components/starting-page/starting-page.jsx
+++ src/components/starting-page/starting-page.jsx
@@ -1,6 +1,6 @@
 import './starting-page.css'
 import React from 'react';
-import { Result, Button } from 'antd';
+import { Result, Button, Card, Row, Col } from 'antd';
 import { SmileOutlined } from '@ant-design/icons';
 
 
@@ -16,11 +16,31 @@ const StartingPage = ({ history, setQ }) => {
         return themes[Math.floor(Math.random() * themes.length)]
     }
 
+    const gridStyle = {
+        width: '25%',
+        textAlign: 'center',
+    };
+
     return (
-        <Result
+
+        <>
+            <Row span={24} className='welcome-page'>
+                <Col span={24}>Welcome to the movie database</Col>
+                <Col span={24} >This app will help you</Col>
+                <Col span={12}>
+                    find the movie you are looking for </Col>
+                <Col span={12}>
+                    find out something new </Col>
+                <Col span={12}>film</Col>
+                <Col span={12}>series</Col>
+            </Row>
+
+        </>
+
+        /* <Result
             icon={<SmileOutlined />}
             title="Welcome to the movie database"
-            subTitle={'This app helps you to find the movie you looking for or explore something new by entering a title and/or date. MovieDB will help it quickly rank movies based on criteria film/series. Then you can check more details about film such as short description, genre, durations, actors list, reviews and other things which is useful if there are people who do not watch films yet! Or just click the button and there will be some random films'}
+            subTitle={'This app helps you to find the movie you looking for by entering a title; or explore something new e. MovieDB will help it quickly rank movies based on criteria film/series. Then you can check more details about film such as short description, genre, durations, actors list, reviews and other things which is useful if there are people who do not watch films yet! Or just click the button and there will be some random films'}
             extra={
                 <Button
                     className='btn-start-page'
@@ -30,6 +50,9 @@ const StartingPage = ({ history, setQ }) => {
                 </Button>
             }
         />
+ */
+
+
     )
 }
 
