fcb4ab9946baa87178fafe9821fa09960d98f30e
Viktoryia Nelia <75176786+vikylkinnelya@users.noreply.github.com>
2021-04-27 15:38:28 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,2 +1,15 @@
-'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(['index.html','style/style.css']); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+self.addEventListener('install', e => {
+    e.waitUntil(
+      // после установки service worker
+      // открыть новый кэш
+      caches.open('my-pwa-cache').then(cache => {
+        // добавляем все URL ресурсов, которые хотим закэшировать
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/about.html',
+          '/static/css/main.6014d180.chunk.css',
+        ]);
+      })
+    );
+   });
